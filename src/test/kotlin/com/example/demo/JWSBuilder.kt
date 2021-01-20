e9e9a863c061df7761ecf466cb7711335c63993a
Kartik Patel <kartik.patel@wwt.com>
2021-01-20 16:01:41 -0500

    Updating JWSBuilder to use Kotlin scoped function

--- src/test/kotlin/com/example/demo/JWSBuilder.kt
+++ src/test/kotlin/com/example/demo/JWSBuilder.kt
@@ -8,49 +8,34 @@ import java.util.UUID
 
 data class JWSBuilder(
     var rsaJsonWebKey: RsaJsonWebKey? = null,
-    var issuer: String? = null,
-    var subject: String? = null) {
+    var claimsIssuer: String? = null,
+    var claimsSubject: String? = null) {
 
     fun rsaJsonWebKey(rsaJsonWebKey: RsaJsonWebKey) = apply { this.rsaJsonWebKey = rsaJsonWebKey }
-    fun issuer(issuer: String) = apply { this.issuer = issuer }
-    fun subject(subject: String) = apply { this.subject = subject }
+    fun issuer(issuer: String) = apply { this.claimsIssuer = issuer }
+    fun subject(subject: String) = apply { this.claimsIssuer = subject }
 
     fun build(): JsonWebSignature {
-        // Create the Claims, which will be the content of the JWT
-        val claims = JwtClaims()
-        claims.setJwtId(UUID.randomUUID().toString()) // a unique identifier for the token
-        claims.setExpirationTimeMinutesInTheFuture(10F) // time when the token will expire (10 minutes from now)
-        claims.setIssuedAtToNow() // when the token was issued/created (now)
-        claims.setAudience("https://host/api") // to whom this token is intended to be sent
-        claims.setIssuer(issuer) // who creates the token and signs it
-        claims.setSubject(subject) // the subject/principal is whom the token is about
-        claims.setClaim("azp", "example-client-id") // Authorized party  (the party to which this token was issued)
-        claims.setClaim("scope", "openid profile email")
+        // The JWT Claims Set represents a JSON object whose members are the claims conveyed by the JWT.
+        val claims = JwtClaims().apply {
+            jwtId = UUID.randomUUID().toString() // unique identifier for the JWT
+            issuer = claimsIssuer // identifies the principal that issued the JWT
+            subject = claimsSubject // identifies the principal that is the subject of the JWT
+            setAudience("https://host/api") // identifies the recipients that the JWT is intended for
+            setExpirationTimeMinutesInTheFuture(10F) // identifies the expiration time on or after which the JWT MUST NOT be accepted for processing
+            setIssuedAtToNow() // identifies the time at which the JWT was issued
+            setClaim("azp", "example-client-id") // Authorized party - the party to which the ID Token was issued
+            setClaim("scope", "openid profile email") // Scope Values
+        }
 
-        // A JWT is a JWS and/or a JWE with JSON claims as the payload.
-        // In this example it is a JWS so we create a JsonWebSignature object.
-        val jws = JsonWebSignature()
+        val jws = JsonWebSignature().apply {
+            payload = claims.toJson()
+            key = rsaJsonWebKey?.getPrivateKey() // the key to sign the JWS with
+            algorithmHeaderValue = rsaJsonWebKey?.algorithm // Set the signature algorithm on the JWT/JWS that will integrity protect the claims
+            keyIdHeaderValue = rsaJsonWebKey?.getKeyId() // a hint indicating which key was used to secure the JWS
+            setHeader("typ", "JWT") // the media type of this JWS
+        }
 
-        // The payload of the JWS is JSON content of the JWT Claims
-        jws.setPayload(claims.toJson())
-
-        // The JWT is signed using the private key
-        jws.setKey(rsaJsonWebKey?.getPrivateKey())
-
-        // Set the Key ID (kid) header because it's just the polite thing to do.
-        // We only have one key in this example but a using a Key ID helps
-        // facilitate a smooth key rollover process
-        jws.setKeyIdHeaderValue(rsaJsonWebKey?.getKeyId())
-
-        // Set the signature algorithm on the JWT/JWS that will integrity protect the claims
-        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256)
-
-        // set the type header
-        jws.setHeader("typ", "JWT")
-
-        // Sign the JWS and produce the compact serialization or the complete JWT/JWS
-        // representation, which is a string consisting of three dot ('.') separated
-        // base64url-encoded parts in the form Header.Payload.Signature
         return jws
     }
 }
