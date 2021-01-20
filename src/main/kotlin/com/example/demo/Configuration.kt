6930657cdad7c50601e46a7c9862fc8f7f9b6fa8
Tom Hermann <tom.hermann@wwt.com>
2021-01-20 17:14:52 -0600

    Use Kotlin DSL for configuring security.

--- src/main/kotlin/com/example/demo/Configuration.kt
+++ src/main/kotlin/com/example/demo/Configuration.kt
@@ -1,24 +1,23 @@
 package com.example.demo
 
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.web.servlet.invoke
 
-@Configuration(proxyBeanMethods = false)
-class Configuration {
-    @Bean
-    fun securityConfigurer(): WebSecurityConfigurerAdapter {
-        return object: WebSecurityConfigurerAdapter() {
-            override fun configure(http: HttpSecurity) {
-                http.authorizeRequests { authz ->
-                    authz
-                        .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .anyRequest().authenticated()
-                }
-                    .oauth2ResourceServer { oauth2 -> oauth2.jwt() }
+@EnableWebSecurity
+class Configuration : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http {
+            authorizeRequests {
+                authorize(HttpMethod.GET, "/actuator/**", permitAll)
+                authorize(HttpMethod.OPTIONS, "/**", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+            oauth2ResourceServer {
+                jwt {}
             }
         }
     }
